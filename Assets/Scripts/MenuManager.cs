70feeb829df1372bce807336e76b190be669a879
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2024-02-16 00:11:57 +1100

    add onboarding ui section

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -11,11 +11,25 @@ public class MenuManager : MonoBehaviour
     {
         circleTransitionAnimator.SetTrigger("Start");
         logoTransitionAnimator.SetTrigger("Start");
+        Invoke("LoadOnboarding", 1.05f);
+    }
+
+    public void StartGameType2()
+    {
+        circleTransitionAnimator.SetTrigger("Start");
+        logoTransitionAnimator.SetTrigger("Start");
+        Time.timeScale = 1;
         Invoke("LoadGameScene", 1.05f);
     }
 
     private void LoadGameScene()
     {
+        Time.timeScale = 1;
         UnityEngine.SceneManagement.SceneManager.LoadScene("GameType2");
+    }
+
+    private void LoadOnboarding()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game2Onboarding");
     }
 }
