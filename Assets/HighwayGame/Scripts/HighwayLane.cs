07302151e2db4fc26946caccb520de2f5aefb446
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2024-02-20 01:13:45 +1100

    implement game type 2 note spawning

---
+++ Assets/HighwayGame/Scripts/HighwayLane.cs
@@ -0,0 +1,85 @@
+﻿using Melanchall.DryWetMidi.Interaction;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighwayLane : MonoBehaviour
+{
+    public Melanchall.DryWetMidi.MusicTheory.NoteName noteRestriction;
+    public KeyCode input;
+    public GameObject notePrefab;
+    List<HighwayNote> notes = new List<HighwayNote>();
+    public List<double> timeStamps = new List<double>();
+
+    int spawnIndex = 0;
+    int inputIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void SetTimeStamps(Melanchall.DryWetMidi.Interaction.Note[] array)
+    {
+        foreach (var note in array)
+        {
+            if (note.NoteName == noteRestriction)
+            {
+                var metricTimeSpan = TimeConverter.ConvertTo<MetricTimeSpan>(note.Time, HighwaySongManager.midiFile.GetTempoMap());
+                timeStamps.Add((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds + (double)metricTimeSpan.Milliseconds / 1000f);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnIndex < timeStamps.Count)
+        {
+            if (HighwaySongManager.GetAudioSourceTime() >= timeStamps[spawnIndex] - HighwaySongManager.Instance.noteTime)
+            {
+                var note = Instantiate(notePrefab, transform);
+                notes.Add(note.GetComponent<HighwayNote>());
+                note.GetComponent<HighwayNote>().assignedTime = (float)timeStamps[spawnIndex];
+                spawnIndex++;
+            }
+        }
+
+        if (inputIndex < timeStamps.Count)
+        {
+            double timeStamp = timeStamps[inputIndex];
+            double marginOfError = HighwaySongManager.Instance.marginOfError;
+            double audioTime = HighwaySongManager.GetAudioSourceTime() - (HighwaySongManager.Instance.inputDelayInMilliseconds / 1000.0);
+
+            if (Input.GetKeyDown(input))
+            {
+                if (Math.Abs(audioTime - timeStamp) < marginOfError)
+                {
+                    Hit();
+                    print($"Hit on {inputIndex} note");
+                    Destroy(notes[inputIndex].gameObject);
+                    inputIndex++;
+                }
+                else
+                {
+                    print($"Hit inaccurate on {inputIndex} note with {Math.Abs(audioTime - timeStamp)} delay");
+                }
+            }
+            if (timeStamp + marginOfError <= audioTime)
+            {
+                Miss();
+                print($"Missed {inputIndex} note");
+                inputIndex++;
+            }
+        }       
+    
+    }
+    private void Hit()
+    {
+        HighwayScoreManager.Hit();
+    }
+    private void Miss()
+    {
+        HighwayScoreManager.Miss();
+    }
+}
---
+++ Assets/HighwayGame/Scripts/HighwayNote.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighwayNote : MonoBehaviour
+{
+    double timeInstantiated;
+    public float assignedTime;
+
+    void Start()
+    {
+        timeInstantiated = HighwaySongManager.GetAudioSourceTime();
+        transform.localPosition = Vector3.forward * HighwaySongManager.Instance.noteSpawnY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double timeSinceInstantiated = HighwaySongManager.GetAudioSourceTime() - timeInstantiated;
+        float t = (float)(timeSinceInstantiated / (HighwaySongManager.Instance.noteTime * 2));
+
+        
+        if (t > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            //transform.localPosition = Vector3.Lerp(Vector3.forward * HighwaySongManager.Instance.noteSpawnY, Vector3.forward * HighwaySongManager.Instance.noteDespawnY, t);
+            transform.Translate(Vector3.forward * (HighwaySongManager.Instance.noteDespawnY - HighwaySongManager.Instance.noteSpawnY) * Time.deltaTime / (HighwaySongManager.Instance.noteTime * 2));
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+}
