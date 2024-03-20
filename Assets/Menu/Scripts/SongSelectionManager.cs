bc8c230298e12219ac707efda96e685536e4ccad
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2024-03-21 04:49:24 +1100

    finish initial part of song select
    implement list generation, animations, audio looping, audio switching

---
+++ Assets/Menu/Scripts/SongSelectionManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongSelectionManager : MonoBehaviour
+{
+    [HideInInspector, Tooltip("Instance of the SongSelectionManager")] public static SongSelectionManager instance;
+    [SerializeField, Tooltip("Reference to the song list shelf")] private GameObject songListShelf;
+    [SerializeField, Tooltip("Reference to the prefab for the song tile")] private GameObject songTilePrefab;
+    [SerializeField, Tooltip("A list of all the songs to populate the menu with")] private List<SongData> songs = new List<SongData>();
+    [HideInInspector, Tooltip("The currently active song tile")] public SongTileManager activeSongTile;
+    [SerializeField, Tooltip("Reference to the audio sources for the song selection menu")] private AudioSource[] audioSources = new AudioSource[2];
+    [HideInInspector, Tooltip("References to all the song tiles")] private List<SongTileManager> songTiles = new List<SongTileManager>();
+    [Tooltip("The currently active audio source index")] private int activeAudioSource = 0;
+    [Tooltip("The next time that audio will start")] private double nextStartTime = 0;
+    [Tooltip("If the song has started looping or not")] private bool isLooping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        PopulateSongList();
+    }
+
+    private void PopulateSongList()
+    {
+        foreach (SongData song in songs)
+        {
+            GameObject songTile = Instantiate(songTilePrefab, songListShelf.transform);
+            SongTileManager songTileManager = songTile.GetComponent<SongTileManager>();
+            songTileManager.songData = song;
+            songTiles.Add(songTileManager);
+        }
+    }
+
+    public void ChangeSong(SongTileManager songTileManager)
+    {
+        if (songTileManager == activeSongTile)
+        {
+            return;
+        }
+        foreach (SongTileManager songTile in songTiles)
+        {
+            songTile.StopBPM();
+        }
+        activeSongTile = songTileManager;
+        CrossfadeSong();
+    }
+
+    private void CrossfadeSong()
+    {
+        isLooping = false;
+        int otherAudioSource = activeAudioSource == 0 ? 1 : 0;
+        if (audioSources[0].clip == null)
+        {
+            audioSources[0].clip = activeSongTile.songData.SongAudio;
+            audioSources[1].clip = activeSongTile.songData.SongAudio;
+        }
+        else
+        {
+            audioSources[otherAudioSource].Stop();
+            audioSources[otherAudioSource].clip = activeSongTile.songData.SongAudio;
+        }
+        audioSources[otherAudioSource].time = activeSongTile.songData.PreviewStart;
+        audioSources[otherAudioSource].PlayScheduled(AudioSettings.dspTime);
+        nextStartTime = AudioSettings.dspTime + (activeSongTile.songData.PreviewEnd - activeSongTile.songData.PreviewStart);
+        audioSources[otherAudioSource].SetScheduledEndTime(nextStartTime);
+        audioSources[activeAudioSource].Stop();
+        audioSources[activeAudioSource].clip = activeSongTile.songData.SongAudio;
+        activeAudioSource = activeAudioSource == 0 ? 1 : 0;
+        isLooping = true;
+    }
+
+    void Update()
+    {
+        if (isLooping && AudioSettings.dspTime >= nextStartTime - 1)
+        {
+            activeAudioSource = activeAudioSource == 0 ? 1 : 0;
+            audioSources[activeAudioSource].time = activeSongTile.songData.LoopPoint;
+            audioSources[activeAudioSource].PlayScheduled(nextStartTime);
+            nextStartTime += activeSongTile.songData.PreviewEnd - activeSongTile.songData.LoopPoint;
+            audioSources[activeAudioSource].SetScheduledEndTime(nextStartTime);
+        }
+    }
+}
--- Assets/Menu/Scripts/SongTileManager.cs
+++ Assets/Menu/Scripts/SongTileManager.cs
@@ -6,7 +6,10 @@ using TMPro;
 
 public class SongTileManager : MonoBehaviour
 {
+    [Header("Song Tile Data")]
     [SerializeField, Tooltip("The song data that this song tile represents")] public SongData songData;
+    [SerializeField, Tooltip("Whether the song is locked or unlocked for the player")] public bool isLocked;
+    [SerializeField, Tooltip("Whether the song is new for the player")] public bool isNew;
     [SerializeField, Tooltip("The animator component for the song tile")] private Animator tileAnimatorController;
     [SerializeField, Tooltip("The Image component that will display the song's album art")] private Image albumArt;
     [SerializeField, Tooltip("The Text component that will display the song's title when hovered")] private TextMeshProUGUI songTitleHover;
@@ -18,21 +21,54 @@ public class SongTileManager : MonoBehaviour
     [SerializeField, Tooltip("The icon for BongoGame compatibility")] private GameObject bongoGameIcon;
     [SerializeField, Tooltip("The icon for telling the player that they have not yet played the song")] private GameObject bangIcon;
     [SerializeField, Tooltip("The icon for telling the player that they cannot play the song yet")] private GameObject lockIcon;
+    [Header("Material Properties")]
+    [SerializeField, Tooltip("Material to use for the song image")] private Material songMaterial;
+    [SerializeField, Tooltip("The intensity of the outer glow effect"), Range(0, 0.55f)] public float outerGlowIntensity = 0.3f;
+    [SerializeField, Tooltip("The power of the outer glow effect"), Range(0, 50f)] public float outerGlowPower = 5f;
+    [SerializeField, Tooltip("The power of the outline effect"), Range(0, 50f)] public float outlinePower = 10f;
+    [Tooltip("Reference to the button component for the song tile")] private Button songTileButton;
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        //Start();
+        albumArt.material = new Material(songMaterial);
+        UpdateMaterial();
+    }
+#endif
 
     private void Start()
     {
+        albumArt.material = new Material(songMaterial);
+        songTileButton = GetComponent<Button>();
         if (songData != null)
         {
             albumArt.sprite = songData.AlbumCover;
             songTitle.text = songData.SongName;
-            //songArtist.text = songData.ArtistName;
-            //songTitleHover.text = songData.SongName;
+            songArtist.text = songData.ArtistName;
+            songTitleHover.text = songData.SongName;
             circleGameIcon.SetActive(false);
             highwayGameIcon.SetActive(false);
             marchingGameIcon.SetActive(false);
             bongoGameIcon.SetActive(false);
-            //bangIcon.SetActive(false);
-            lockIcon.SetActive(false);
+            if (isNew)
+            {
+                bangIcon.SetActive(true);
+            }
+            else
+            {
+                bangIcon.SetActive(false);
+            }
+            if (isLocked)
+            {
+                lockIcon.SetActive(true);
+                songTileButton.interactable = false;
+            }
+            else
+            {
+                lockIcon.SetActive(false);
+                songTileButton.interactable = true;
+            }
             foreach (GameMode gameMode in songData.GameModes)
             {
                 switch (gameMode.GameType)
@@ -51,6 +87,61 @@ public class SongTileManager : MonoBehaviour
                         break;
                 }
             }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        UpdateMaterial();
+    }
+
+    private void UpdateMaterial()
+    {
+        if (albumArt != null)
+        {
+            albumArt.materialForRendering.SetFloat("_Intensity", outerGlowIntensity);
+            albumArt.materialForRendering.SetFloat("_Power", outerGlowPower);
+            albumArt.materialForRendering.SetFloat("_OutlinePower", outlinePower);
+            if (isLocked)
+            {
+                albumArt.materialForRendering.SetInt("_isLocked", 1);
+            }
+            else
+            {
+                albumArt.materialForRendering.SetInt("_isLocked", 0);
+            }
+        }
+    }
+
+    public void OnHover()
+    {
+        if (tileAnimatorController != null && tileAnimatorController.GetBool("AnimateWithBPM") == false && songData.Bpm > 0)
+        {
+            SongSelectionManager.instance.ChangeSong(this);
+            tileAnimatorController.SetBool("AnimateWithBPM", true);
+            tileAnimatorController.SetFloat("BPMMultiplier", songData.Bpm / 60f);
+        }
+    }
+
+    public void StartBPM()
+    {
+        tileAnimatorController.SetBool("ResetBPM", true);
+    }
+
+    public void OnExit()
+    {
+        if (tileAnimatorController != null)
+        {
+            return;
+        }
+    }
+
+    public void StopBPM()
+    {
+        if (tileAnimatorController != null)
+        {
+            tileAnimatorController.SetBool("AnimateWithBPM", false);
+            tileAnimatorController.SetBool("ResetBPM", false);
         }
     }
 }
