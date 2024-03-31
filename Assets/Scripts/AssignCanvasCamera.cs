4f31e1d0784d47a40c2ca43b8f063579c5029290
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2024-04-01 00:57:28 +1100

    implement cursor + cursor changing on hover of clickable ui
    unfortunately its limited to 32x32 in size so its really tiny :(
    also add persistent scene for cursor controller + other future functionality

--- Assets/Scripts/AssignCanvasCamera.cs
+++ Assets/Scripts/AssignCanvasCamera.cs
@@ -6,16 +6,35 @@ public class AssignCanvasCamera : MonoBehaviour
 {
     [Tooltip("Reference to the canvas to assign the camera to.")] private Canvas canvas;
 
-    private void Start()
+    private void Awake()
     {
         canvas = GetComponent<Canvas>();
         canvas.worldCamera = Camera.main;
+    }
+
+    private IEnumerator Start()
+    {
+        canvas = GetComponent<Canvas>();
+        yield return null;
+        canvas.worldCamera = Camera.main;
+        yield return null;
     }
 
     private void OnEnable()
     {
         canvas = GetComponent<Canvas>();
         canvas.worldCamera = Camera.main;
+        StartCoroutine(RefreshCamera());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
     }
 
     private void OnApplicationPause(bool pauseStatus)
@@ -26,5 +45,14 @@ public class AssignCanvasCamera : MonoBehaviour
     private void OnApplicationFocus(bool focusStatus)
     {
         canvas.worldCamera = Camera.main;
+    }
+
+    private IEnumerator RefreshCamera()
+    {
+        while (true)
+        {
+            canvas.worldCamera = Camera.main;
+            yield return new WaitForSeconds(1.5f);
+        }
     }
 }
