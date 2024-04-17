e8110f495e791083d2c31b45d481cdab20832e1a
Alex Hanson <43489279+dippyshere@users.noreply.github.com>
2024-04-18 00:25:36 +1000

    Show mobile UI in simulator + hide keybind prompts for mobile

--- Assets/Menu/Scripts/MobileOnlyUI.cs
+++ Assets/Menu/Scripts/MobileOnlyUI.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.InputSystem;
 
 public class MobileOnlyUI : MonoBehaviour
 {
     [SerializeField, Tooltip("The image to enable when the game is running on mobile")] private Image mobileOnlyImage;
+    [SerializeField, Tooltip("The sprite to enable when the game is running on mobile")] private SpriteRenderer mobileOnlySprite;
 
     void Start()
     {
-        if (Application.isMobilePlatform)
+        if (UnityEngine.Device.SystemInfo.deviceType != DeviceType.Desktop)
         {
+            if (mobileOnlyImage != null)
             mobileOnlyImage.enabled = true;
+            if (mobileOnlySprite != null)
+            mobileOnlySprite.enabled = true;
         }
         else
         {
+            if (mobileOnlyImage != null)
             mobileOnlyImage.enabled = false;
+            if (mobileOnlySprite != null)
+            mobileOnlySprite.enabled = false;
         }
     }
 }
